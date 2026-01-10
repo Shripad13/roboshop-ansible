f2f381bbfce6fc607055233c0a023a9fa79d6eea
Shripad13 <stmaddi@outlook.com>
2026-01-10 16:42:07 +0530

    update the file

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@ git pull
 
 if [ -z "$1" -o -z "$2" -o -z "$3"]; then
     echo -e "\e[31m Input is missing\e[0m]"
-    echo "Expected Usage: \n\t\t bash init.sh frontend dev PWD"
+    echo "Expected Usage: \n\t\t bash init.sh frontend dev PWD vault_token"
     exit 89
 fi
 
@@ -10,10 +10,11 @@ fi
 COMPONENT=$1
 ENV=$2
 SSH_PASSWORD=$3
+vault_token=$4
 
 #ansible-playbook -i ${COMPONENT}-${ENV} -e ansible_user=ec2-user -e ansible_password=DevOps321 -e COMPONENT=${COMPONENT} -e ENV=${ENV} expense.yml
 
-ansible-playbook -i ${COMPONENT}-${ENV}.devsecopswithshri.site, -e ansible_user=ec2-user -e ansible_password=${SSH_PASSWORD} -e COMPONENT=${COMPONENT} -e ENV=${ENV} expense.yml
+ansible-playbook -i ${COMPONENT}-${ENV}.devsecopswithshri.site, -e ansible_user=ec2-user -e ansible_password=${SSH_PASSWORD} -e COMPONENT=${COMPONENT} -e ENV=${ENV} -e vault_token=${vault_token} expense.yml
 
 
 
